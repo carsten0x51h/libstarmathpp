065791c5583780f8c7d53b7ebffbd317a5ed9669
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2024-05-18 21:09:07 +0200

    refactor: Removed include.

--- libstarmathpp/io/image_reader.test.cpp
+++ libstarmathpp/io/image_reader.test.cpp
@@ -31,7 +31,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
-#include <boost/test/tools/floating_point_comparison.hpp>
+//#include <boost/test/tools/floating_point_comparison.hpp>
 
 #include <libstarmathpp/io/image_reader.hpp>
 
@@ -41,46 +41,46 @@ namespace bdata = boost::unit_test::data;
  *
  */
 BOOST_DATA_TEST_CASE(fits_image_reader_test,
-        bdata::make(
-            std::vector<std::string> {
-                "test_data/image_reader/test_image_fits_496x380.fit",
-                "test_data/image_reader/test_image_fits_496x380.FIT",
-                "test_data/image_reader/test_image_fits_496x380.fits",
-                "test_data/image_reader/test_image_fits_496x380.Fits",
-                "test_data/image_reader/test_image_fits_496x380.FITS",
-                "test_data/image_reader/test_image_fits_496x380.fit.gz",
-                "test_data/image_reader/test_image_fits_496x380.FIT.GZ",
-                "test_data/image_reader/test_image_fits_496x380.fits.gz",
-                "test_data/image_reader/test_image_fits_496x380.fits.GZ",
-                "test_data/image_reader/test_image_fits_496x380.Fits.GZ",
-                "test_data/image_reader/test_image_fits_496x380.FITS.GZ",
-                "test_data/image_reader/test_image_16bit_100x100.tif",
-                "test_data/image_reader/test_image_32bit_120x120.tif"
-            }) ^
-        bdata::make(
-            // img width, img height, pixel x pos, pixel y pos, exp. pixel value
-            std::vector< std::tuple<int, int, int, int, float> > {
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 496, 380, 431, 203, 65535.0F },
-                { 100, 100,   0,   0, 65535.0F }, // Img should have 16 bit, each pixel should have pixel value 65535.
-                { 120, 120,   0,   0,     0.5F }, // Img should have 32 bit, each pixel should have pixel value 0.5.
-            }),
-        image_filename, image_width, image_height, pos_x, pos_y, pixel_value)
+    bdata::make(
+        std::vector<std::string> {
+          "test_data/image_reader/test_image_fits_496x380.fit",
+          "test_data/image_reader/test_image_fits_496x380.FIT",
+          "test_data/image_reader/test_image_fits_496x380.fits",
+          "test_data/image_reader/test_image_fits_496x380.Fits",
+          "test_data/image_reader/test_image_fits_496x380.FITS",
+          "test_data/image_reader/test_image_fits_496x380.fit.gz",
+          "test_data/image_reader/test_image_fits_496x380.FIT.GZ",
+          "test_data/image_reader/test_image_fits_496x380.fits.gz",
+          "test_data/image_reader/test_image_fits_496x380.fits.GZ",
+          "test_data/image_reader/test_image_fits_496x380.Fits.GZ",
+          "test_data/image_reader/test_image_fits_496x380.FITS.GZ",
+          "test_data/image_reader/test_image_16bit_100x100.tif",
+          "test_data/image_reader/test_image_32bit_120x120.tif"
+        }) ^
+    bdata::make(
+        // img width, img height, pixel x pos, pixel y pos, exp. pixel value
+        std::vector< std::tuple<int, int, int, int, float> > {
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 496, 380, 431, 203, 65535.0F},
+          { 100, 100, 0, 0, 65535.0F},  // Img should have 16 bit, each pixel should have pixel value 65535.
+          { 120, 120, 0, 0, 0.5F},  // Img should have 32 bit, each pixel should have pixel value 0.5.
+        }),
+    image_filename, image_width, image_height, pos_x, pos_y, pixel_value)
 {
-    auto img_ptr = starmathpp::io::read(image_filename);
+  auto img_ptr = starmathpp::io::read(image_filename);
 
-    DEBUG_IMAGE_DISPLAY(*img_ptr, "image_reader_test", 0);
+  DEBUG_IMAGE_DISPLAY(*img_ptr, "image_reader_test", 0);
 
-    BOOST_TEST(img_ptr->width() == image_width);
-    BOOST_TEST(img_ptr->height() == image_height);
-    BOOST_CHECK_CLOSE((*img_ptr)(pos_x, pos_y), pixel_value, 0.001F);
+  BOOST_TEST(img_ptr->width() == image_width);
+  BOOST_TEST(img_ptr->height() == image_height);
+  BOOST_CHECK_CLOSE((*img_ptr)(pos_x, pos_y), pixel_value, 0.001F);
 }
