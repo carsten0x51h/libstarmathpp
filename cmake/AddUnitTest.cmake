a207122d334225d5110724760a425a60b7a8e869
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2024-05-18 22:32:05 +0200

    refactor: Adding cmake macro to add test modules.

---
+++ cmake/AddUnitTest.cmake
@@ -0,0 +1,12 @@
+macro(add_test_module TEST_MODULE_NAME TEST_SOURCES)
+   message("* Adding test module ${TEST_MODULE_NAME}...")
+   add_executable(${TEST_MODULE_NAME} ${TEST_SOURCES})
+   target_link_libraries(${TEST_MODULE_NAME}
+	PRIVATE
+	${target}
+	${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+	${DEFAULT_LINKER_OPTIONS}
+   )
+   add_test(NAME ${TEST_MODULE_NAME} COMMAND ${TEST_MODULE_NAME} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+endmacro()
