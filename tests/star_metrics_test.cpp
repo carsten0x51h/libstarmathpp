50f82ca2486257d2b2dc22a9db376fbd3125a87f
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2024-05-30 21:22:45 +0200

    test: Add integration test for star metrics.

--- tests/star_metrics_test.cpp
+++ tests/star_metrics_test.cpp
@@ -29,6 +29,10 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 
+#include <tuple>
+#include <vector>
+#include <cmath>
+
 #include <boost/test/unit_test.hpp>
 
 #include <range/v3/range/conversion.hpp>
@@ -37,8 +41,6 @@
 #include <range/v3/view/filter.hpp>
 #include <range/v3/core.hpp>   // ranges::front()
 #include <range/v3/algorithm/for_each.hpp>
-
-
 
 #include <libstarmathpp/pipeline/views/files.hpp>
 #include <libstarmathpp/pipeline/views/read.hpp>
@@ -54,6 +56,7 @@
 #include <libstarmathpp/algorithm/threshold/otsu_thresholder.hpp>
 #include <libstarmathpp/algorithm/centroid/intensity_weighted_centroider.hpp>
 #include <libstarmathpp/algorithm/snr.hpp>
+#include <libstarmathpp/algorithm/hfd.hpp>
 
 #include <libstarmathpp/floating_point_equality.hpp>
 #include <libstarmathpp/size.hpp>
@@ -65,6 +68,15 @@ using namespace starmathpp;
 using namespace starmathpp::io;
 using namespace starmathpp::algorithm;
 using namespace starmathpp::pipeline::views;
+
+/**
+ *
+ */
+bool are_tuples_equal(const std::tuple<float, float> &t1,
+                      const std::tuple<float, float> &t2, float tolerance) {
+  return std::fabs(std::get<0>(t1) - std::get<0>(t2)) < tolerance
+      && std::fabs(std::get<1>(t1) - std::get<1>(t2)) < tolerance;
+}
 
 /**
  * TODO: Add interpolate bad pixels...
@@ -79,7 +91,7 @@ using namespace starmathpp::pipeline::views;
  *       FwhmT fwhmObj(ImageSlicerT::slice<SliceDirectionT::VERT>(*result, result->width()/2));
  * TODO: Change: pass in entire image -> calculate mean of VERT + HORZ Fwhm and return this as value. FwhmT should provide functions to get both separately.
  */
-BOOST_AUTO_TEST_CASE(pipeline_star_metrics_test, * boost::unit_test::tolerance(0.1))
+BOOST_AUTO_TEST_CASE(pipeline_star_metrics_test, * boost::unit_test::tolerance(0.5))
 {
   std::vector<std::string> image_paths {
     "test_data/integration/star_metrics/newton_focus_star1.tiff",
@@ -97,20 +109,20 @@ BOOST_AUTO_TEST_CASE(pipeline_star_metrics_test, * boost::unit_test::tolerance(0
 
   std::vector<std::tuple<double, double>> expected_star_metrics = {
     { 3.4248103734966566, 27.295144884427497},
-    { 4.0168380799027004, 25.372589848114831},
+    { 4.0168380799027004, 25.431775505128925},
     { 4.2398086088788904, 23.906962737212321},
-    { 4.5106426505897472, 21.773008162322135},
+    { 4.5106426505897472, 21.797925759551219},
     { 5.9366687230635415, 18.646887642682195},
-    { 5.9214817393646637, 15.807635535535937},
+    { 5.9214817393646637, 15.900437967399453},
     { 7.2058655558692744, 12.081075787803762},
     { 6.7269579863995439, 9.182079739644923},
     { 7.4242741594713344, 6.2974607834559153},
-    { 5.278791273812149, 3.5406091827173412},
-    { 4.1407320775829746, 2.7410637875414858}
+    { 5.278791273812149, 3.8438485006311049},
+    { 4.1407320775829746, 3.4434783057891602}
   };
 
   auto star_metrics =
-      image_paths
+  image_paths
   | read()
 //  | view::filter(& metrics::is_not_saturated)
   | subtract_background(OtsuThresholder<float>(16))
@@ -119,13 +131,21 @@ BOOST_AUTO_TEST_CASE(pipeline_star_metrics_test, * boost::unit_test::tolerance(0
   | scale_down(3.0F)
   | starmathpp::pipeline::views::crop_from_center(Size<int>(61,61))
   | view::transform(
-      [](const auto & imgPtr) {
-        return std::make_tuple(starmathpp::algorithm::snr(*imgPtr), 1.0/*, HfdT(*imgPtr).getValue()*/);
+      [](const auto & img_ptr) {
+        return std::make_tuple(
+            starmathpp::algorithm::snr(*img_ptr),
+            starmathpp::algorithm::hfd(*img_ptr)
+        );
       })
   | to<std::vector>();
 
-// TODO: Use test close?
-//  BOOST_TEST(star_metrics == expected_star_metrics);
+  float tolerance = 0.001F;
+
+  BOOST_REQUIRE_EQUAL(star_metrics.size(), expected_star_metrics.size());
+
+  for (size_t i = 0; i < star_metrics.size(); ++i) {
+    BOOST_CHECK(are_tuples_equal(star_metrics[i], expected_star_metrics[i], tolerance));
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END();
