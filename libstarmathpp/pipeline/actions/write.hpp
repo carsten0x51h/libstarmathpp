be95a680041c827f93e28bdc3e17bf1176afcc4f
Carsten Schmitt <carsten.schmitt@lost-infinity.com>
2024-06-14 23:44:14 +0200

    feat: Add pipeline write action.

---
+++ libstarmathpp/pipeline/actions/write.hpp
@@ -0,0 +1,105 @@
+/*****************************************************************************
+ *
+ *  libstarmathpp - A C++ library to process astronomical images
+ *                  based on CImg and range-v3.
+ *
+ *  Copyright(C) 2023 Carsten Schmitt <c [at] lost-infinity.com>
+ *
+ *  More info on https://www.lost-infinity.com
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ ****************************************************************************/
+
+#ifndef STARMATHPP_PIPELINE_ACTION_WRITE_H_
+#define STARMATHPP_PIPELINE_ACTION_WRITE_H_ STARMATHPP_PIPELINE_ACTION_WRITE_H_
+
+#include <cstdio>
+#include <filesystem>
+
+#include <range/v3/all.hpp> // TODO: REMOVE - Needed for rages::forward
+
+#include <range/v3/action/transform.hpp>
+
+#include <libstarmathpp/image.hpp>
+#include <libstarmathpp/io/image_writer.hpp>
+
+#define STARMATHPP_PIPELINE_ACTION_WRITE_DEBUG 0
+
+namespace starmathpp::pipeline::actions {
+  /**
+   * NOTE: With C++20, the filename can be easily composed like:
+   * std::string filename = std::format(imageFilenamePattern, counter++);
+   * The template looks slightly different: "img_{:03}.fit"
+   * See https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+   *
+   * #include <format>
+   *
+   * TODO: Compose filename from internal, static counter... ? Better idea? How to reset?
+   */
+  static std::string compose_filename_from_pattern(const std::string &pattern) {
+    char buffer[100];
+
+    static unsigned int counter = 0;
+
+    std::snprintf(buffer, sizeof(buffer), pattern.c_str(), counter++);
+
+    return std::string(buffer);
+  }
+
+  /**
+   *
+   */
+  template<typename ImageType>
+  auto write(const std::filesystem::path &directory,
+             const std::string &image_filename_pattern = "img_%03d.fit",
+             bool allow_override = true) {
+
+    return ranges::make_action_closure([directory, image_filename_pattern, allow_override](auto&& range) {
+        int index = 0;
+        for (auto&& image : range) {
+//            std::ostringstream oss;
+//            oss << directory << '/' << fmt::format(format, index++) << ".jpeg";
+//            image->save(oss.str().c_str());
+                    DEBUG_IMAGE_DISPLAY(*image, "pipeline_action_write_in",
+                                        STARMATHPP_PIPELINE_ACTION_WRITE_DEBUG);
+
+                    std::filesystem::path filepath = directory
+                        / compose_filename_from_pattern(image_filename_pattern);
+
+                    starmathpp::io::write(*image, filepath, allow_override);
+
+        }
+        return std::forward<decltype(range)>(range); // Ensure we forward the range correctly
+    });
+//    return ranges::actions::transform(
+//        [=](const std::shared_ptr<cimg_library::CImg<ImageType>> &image) {
+//
+//          DEBUG_IMAGE_DISPLAY(*image, "pipeline_action_write_in",
+//                              STARMATHPP_PIPELINE_ACTION_WRITE_DEBUG);
+//
+//          std::filesystem::path filepath = directory
+//              / compose_filename_from_pattern(image_filename_pattern);
+//
+//          starmathpp::io::write(*image, filepath, allow_override);
+//
+//          return image;
+//        }
+//    );
+}
+
+}  // namespace starmathpp::pipeline::actions
+
+#endif // STARMATHPP_PIPELINE_ACTION_WRITE_H_
--- libstarmathpp/pipeline/views/write.hpp
+++ libstarmathpp/pipeline/views/write.hpp
@@ -23,31 +23,19 @@
  *
  ****************************************************************************/
 
-#ifndef STARMATHPP_PIPELINE_IO_WRITE_H_
-#define STARMATHPP_PIPELINE_IO_WRITE_H_ STARMATHPP_PIPELINE_IO_WRITE_H_
+#ifndef STARMATHPP_PIPELINE_VIEW_WRITE_H_
+#define STARMATHPP_PIPELINE_VIEW_WRITE_H_ STARMATHPP_PIPELINE_VIEW_WRITE_H_
 
 #include <cstdio>
 #include <filesystem>
 
-/**
- * NOTE: With C++20, the filename can be easily composed like:
- * std::string filename = std::format(imageFilenamePattern, counter++);
- * The template looks slightly different: "img_{:03}.fit"
- * See https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
- */
-//#include <format>
 #include <range/v3/view/transform.hpp>
 
 #include <libstarmathpp/image.hpp>
 #include <libstarmathpp/io/image_writer.hpp>
 
-#define STARMATHPP_PIPELINE_WRITE_DEBUG 0
+#define STARMATHPP_PIPELINE_VIEW_WRITE_DEBUG 0
 
-// TODO: Question: This can be an action or a view... If a view, it just stores
-//       the image "along the way" as one more step in the pipeline.
-//       However, if it is for example supposed to be the last step of a
-//       pipeline, then it should be an action which actively queries
-//       all elements of the pipeline.
 namespace starmathpp::pipeline::views {
 
 /**
@@ -55,6 +43,8 @@ namespace starmathpp::pipeline::views {
  * std::string filename = std::format(imageFilenamePattern, counter++);
  * The template looks slightly different: "img_{:03}.fit"
  * See https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+ *
+ * #include <format>
  *
  * TODO: Compose filename from internal, static counter... ? Better idea? How to reset?
  */
@@ -69,13 +59,7 @@ static std::string compose_filename_from_pattern(const std::string &pattern) {
 }
 
 /**
- * TODO: write is not a view! It is an action! It must not be lazy!
- *       -> Using transform() here is potentially wrong... because if we call write() as last step
- *          of a processing pipeline, nothing will happen, if there is no final "to<vector>()" or
- *          similar....
- *          --> write() can be designed as view AND as action -> depending on what is desired. The
- *              views::write() call can be useful to store a copy to a file in the middle of a pipeline.
- *              actions::write() must be used, as a final step to finish a pipeline.
+ *
  */
 template<typename ImageType>
 auto write(const std::filesystem::path &directory,
@@ -84,8 +68,8 @@ auto write(const std::filesystem::path &directory,
   return ranges::views::transform(
       [=](const std::shared_ptr<cimg_library::CImg<ImageType>> &image) {
 
-        DEBUG_IMAGE_DISPLAY(*image, "pipeline_write_in",
-                            STARMATHPP_PIPELINE_WRITE_DEBUG);
+        DEBUG_IMAGE_DISPLAY(*image, "pipeline_view_write_in",
+                            STARMATHPP_PIPELINE_VIEW_WRITE_DEBUG);
 
         std::filesystem::path filepath = directory
             / compose_filename_from_pattern(image_filename_pattern);
@@ -96,6 +80,7 @@ auto write(const std::filesystem::path &directory,
       }
   );
 }
-}
+} // namespace starmathpp::pipeline::views
 
-#endif /* STARMATHPP_PIPELINE_IO_WRITE_H_ */
+
+#endif // STARMATHPP_PIPELINE_VIEW_WRITE_H_
